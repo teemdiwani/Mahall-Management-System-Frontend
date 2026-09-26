ffb8cb7dbf921857995195ee2f98d5d3a92c2808
teemdiwani <teemdiwani@gmail.com>
2026-09-26 08:50:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: process.env.VITE_API_TARGET || 'http://localhost:5000',
+        target: process.env.VITE_API_TARGET || 'https://mahall-management-system-backend.onrender.com',
         changeOrigin: true,
         secure: false,
       },
